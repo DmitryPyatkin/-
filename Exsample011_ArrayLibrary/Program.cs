9fe56274794bcdd00e3809e07fe3421b9f02f530
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2022-12-04 18:36:22 +0700

    add Exsampel 011 Array Library

---
+++ Exsample011_ArrayLibrary/Program.cs
@@ -0,0 +1,27 @@
+﻿void FillArray(int[] collection) // void, что метод ничего не возвращает.
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length)
+    {
+        collection[index] = new Random().Next(1, 10); // Random, это генератор псевдослучайных чисел. 
+        index++;
+    }
+}
+
+
+void PrintArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while (position < count)
+    {
+        Console.WriteLine(col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
