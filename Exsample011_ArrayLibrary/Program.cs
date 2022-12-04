4a8508344ebd7f10525596d5f63bcbbe3069e9cd
Dmitry Pyatkin <dmitrypyatkin17@gmail.com>
2022-12-04 21:03:40 +0700

    add Exsample 011 Array Library

--- Exsample011_ArrayLibrary/Program.cs
+++ Exsample011_ArrayLibrary/Program.cs
@@ -21,7 +21,32 @@ void PrintArray(int[] col)
     }
 }
 
+int IndexOf(int[] collection, int find)
+{
+    int count = collection.Length;
+    int index = 0;
+    int position = -1;
+    while (index < count)
+    {
+        if(collection[index] == find)
+        {
+            position = index;
+            break;
+        }
+        index++;
+    }
+    return position;
+}
+
 int[] array = new int[10];
 
 FillArray(array);
+array[4] = 4;
+array[6] = 4;
+
+FillArray(array);
 PrintArray(array);
+Console.WriteLine();
+
+int pos = IndexOf(array, 212);
+Console.WriteLine(pos);
